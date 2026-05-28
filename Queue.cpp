d7943167e188802b723e26cf02cfc521aff62e2a
baihaqiramadhani-11 <baihakiramadhani66@gmail.com>
2026-05-28 17:44:52 +0700

    void remove

--- Queue.cpp
+++ Queue.cpp
@@ -47,4 +47,12 @@ public:
         queue_array[REAR] = num;
     }
 
-    
+    void remove()
+    {
+        // ccek apakah antrian kosong
+        if (FRONT == -1)
+        {
+            cout << "Queue underflow\n";
+            return;
+        }
+        cout << "\nThe element deleted from the queue is: " << queue_array[FRONT] << "\n";
